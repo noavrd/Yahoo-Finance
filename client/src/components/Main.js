2ab91b7934464e696a82dca4b21010d17e781396
noavrd <noavrd@gmail.com>
2022-01-12 12:39:49 +0200

    add carousel

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import Logout from './Logout';
 import SingleMarket from './SingleMarket';
 
@@ -17,55 +18,78 @@ export default function Main() {
       .then((result) => setData(result.data.marketSummaryResponse.result))
       .catch((err) => console.log(err));
   }, []);
-
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+      // slidesToSlide: 1, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      // slidesToSlide: 1, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      // slidesToSlide: 1, // optional, default to 1.
+    },
+  };
   console.log(data);
   return (
     <div>
       <div className="all-market">
         {console.log(data)}
-        {data &&
-          data.map((single, i) => (
-            <SingleMarket
-              key={i}
-              exchange={single.exchange}
-              exchangeDataDelayedBy={single.exchangeDataDelayedBy}
-              exchangeTimezoneName={single.exchangeTimezoneName}
-              exchangeTimezoneShortName={single.exchangeTimezoneShortName}
-              firstTradeDateMilliseconds={single.firstTradeDateMilliseconds}
-              fullExchangeName={single.fullExchangeName}
-              gmtOffSetMilliseconds={single.gmtOffSetMilliseconds}
-              language={single.language}
-              market={single.market}
-              marketState={single.marketState}
-              priceHint={single.priceHint}
-              quoteSourceName={single.quoteSourceName}
-              quoteType={single.quoteType}
-              region={single.region}
-              regularMarketChangeFMT={single.regularMarketChange.fmt}
-              regularMarketChangeRAW={single.regularMarketChange.raw}
-              regularMarketChangePercentFMT={
-                single.regularMarketChangePercent.fmt
-              }
-              regularMarketChangePercentRAW={
-                single.regularMarketChangePercent.raw
-              }
-              regularMarketPreviousCloseFMT={
-                single.regularMarketPreviousClose.fmt
-              }
-              regularMarketPreviousCloseRAW={
-                single.regularMarketPreviousClose.raw
-              }
-              regularMarketPriceFMT={single.regularMarketPrice.fmt}
-              regularMarketPriceRAW={single.regularMarketPrice.raw}
-              regularMarketTimeFMT={single.regularMarketTime.fmt}
-              regularMarketTimeRAW={single.regularMarketTime.raw}
-              shortName={single.shortName}
-              sourceInterval={single.sourceInterval}
-              symbol={single.symbol}
-              tradeable={single.tradeable}
-              triggerable={single.triggerable}
-            />
-          ))}
+        {data && (
+          <Carousel
+            responsive={responsive}
+            infinite={true}
+            keyBoardControl={true}>
+            {data &&
+              data.map((single, i) => (
+                <SingleMarket
+                  key={i}
+                  exchange={single.exchange}
+                  exchangeDataDelayedBy={single.exchangeDataDelayedBy}
+                  exchangeTimezoneName={single.exchangeTimezoneName}
+                  exchangeTimezoneShortName={single.exchangeTimezoneShortName}
+                  firstTradeDateMilliseconds={single.firstTradeDateMilliseconds}
+                  fullExchangeName={single.fullExchangeName}
+                  gmtOffSetMilliseconds={single.gmtOffSetMilliseconds}
+                  language={single.language}
+                  market={single.market}
+                  marketState={single.marketState}
+                  priceHint={single.priceHint}
+                  quoteSourceName={single.quoteSourceName}
+                  quoteType={single.quoteType}
+                  region={single.region}
+                  regularMarketChangeFMT={single.regularMarketChange.fmt}
+                  regularMarketChangeRAW={single.regularMarketChange.raw}
+                  regularMarketChangePercentFMT={
+                    single.regularMarketChangePercent.fmt
+                  }
+                  regularMarketChangePercentRAW={
+                    single.regularMarketChangePercent.raw
+                  }
+                  regularMarketPreviousCloseFMT={
+                    single.regularMarketPreviousClose.fmt
+                  }
+                  regularMarketPreviousCloseRAW={
+                    single.regularMarketPreviousClose.raw
+                  }
+                  regularMarketPriceFMT={single.regularMarketPrice.fmt}
+                  regularMarketPriceRAW={single.regularMarketPrice.raw}
+                  regularMarketTimeFMT={single.regularMarketTime.fmt}
+                  regularMarketTimeRAW={single.regularMarketTime.raw}
+                  shortName={single.shortName}
+                  sourceInterval={single.sourceInterval}
+                  symbol={single.symbol}
+                  tradeable={single.tradeable}
+                  triggerable={single.triggerable}
+                />
+              ))}
+          </Carousel>
+        )}
       </div>
     </div>
   );
