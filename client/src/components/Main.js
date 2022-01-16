c55c0024b9ea7706eb8b59b3cc3389e6a5907459
noavrd <noavrd@gmail.com>
2022-01-16 14:16:12 +0200

    fix css for tablets

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -33,7 +33,7 @@ export default function Main() {
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 3,
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
